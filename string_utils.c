d4b7940453e0b8521dc8000320e056603e64cb68
Rubén BM <150420895+WeBeRATAS@users.noreply.github.com>
2024-08-25 13:10:23 +0200

    Add files via upload
    Fractol 25 agosto funciona rrevisar la tecla mas que no funciona el teclado numerico con lo que revisar eso

---
+++ string_utils.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   string_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbuitrag <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/24 14:28:25 by rbuitrag          #+#    #+#             */
+/*   Updated: 2024/08/25 12:18:24 by rbuitrag         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+int	ft_strncmp(char *s1, char *s2, int n)
+{
+	if (s1 == NULL || s2 == NULL || n <=0)
+		return (0);
+	while (*s1 == *s2 && n > 0 && *s1 != '\0')
+	{
+		++s1;
+		++s2;
+		--n;
+	}
+	return (*s1 - *s2);
+}
+
+void	putstr_fd(char *s, int fd)
+{
+	if (s == NULL || fd < 0)
+		return ;
+	if (*s != '\0')
+	{
+		write(fd, s, 1);
+		putstr_fd(s + 1, fd);
+	}
+}
+
+double	atoidbl(char *s)
+{
+	long	integer_part;
+	double	fractional_part;
+	double	pow;
+	int		sign;
+
+	integer_part = 0;
+	fractional_part = 0;
+	sign = +1;
+	pow = 1;
+	while ((*s >= 9 && *s <= 13) || *s == 32)
+		++s;
+	while (*s == '+' || *s == '-')
+		if (*s++ == '-')
+			sign = -sign;
+	while (*s != '.' && *s)
+		integer_part = (integer_part * 10) + (*s++ - 48);
+	if (*s == '.')
+		++s;
+	while (*s)
+	{
+		pow /= 10;
+		fractional_part = fractional_part + (*s++ - 48) * pow;
+	}
+	return ((integer_part + fractional_part) * sign);
+}
